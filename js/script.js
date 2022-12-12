a21b4055c4301262aefbbe12202809817b8ea0cd
GuglielmoBini <guglielmobini@gmail.com>
2022-12-12 16:48:51 +0100

    svolto esercizio 1

--- js/script.js
+++ js/script.js
@@ -25,3 +25,38 @@ L'esito del controllo deve essere stampato in pagina
 
 -----------------------------------------------------------------
 */
+
+// ESERCIZIO 1
+/* STEP
+1- Prendo gli elementi dal DOM
+2- aggiungo event listener al bottone
+3- creo due costanti per i numeri random (player e cpu).
+4- stabilisco quale dei due numeri random è più alto.
+5- stampo in pagina i due numeri random e il vincitore.
+*/
+//***** Prendeo gli elementi dal DOM *****
+const diceButton = document.getElementById("b-dice");
+const playerNumber = document.getElementById("player-number");
+const cpuNumber = document.getElementById("cpu-number");
+const winner = document.getElementById("winner");
+
+let result = "";
+
+diceButton.addEventListener("click", function () {
+  // numero random player
+  const playerRandomNumber = Math.floor(Math.random() * 6) + 1;
+  playerNumber.innerText = playerRandomNumber;
+  // numero random cpu
+  const cpuRandomNumber = Math.floor(Math.random() * 6) + 1;
+  cpuNumber.innerText = cpuRandomNumber;
+  // calcolo il vincitore
+  if (playerRandomNumber > cpuRandomNumber) {
+    result = "Player 1";
+  } else if (playerRandomNumber < cpuRandomNumber) {
+    result = "Cpu";
+  } else {
+    result = "Pareggio";
+  }
+
+  winner.innerText = result;
+});
